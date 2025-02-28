50135f4533e13ad9e9b79b9fc1bd7b7b4cb0493f
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-28 20:46:54 +0100

    feat: wishlist

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,18 +1,19 @@
 import ScrollSmooth from "@/components/ui/scroll-smooth";
 import type { Metadata } from "next";
-import Footer from "@/components/blocks/footer";
-import Header from "@/components/blocks/header";
+/* import Footer from "@/components/blocks/footer";
+import Header from "@/components/blocks/header"; */
 export const metadata: Metadata = {
-  title: "Speetch",
-  description: "Speetch is a platform that allows you to create and manage your clients, projects, and tasks.",
+  title: "Speetly",
+  description: "Speetly is a platform that allows you to create and manage your clients, projects, and tasks.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ScrollSmooth duration={2}>
-      <Header />
+      {/* <Header /> */}
+      
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </ScrollSmooth>
   );
 }
--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,16 +1,34 @@
-import FeaturesSection from "@/components/blocks/features";
-import Hero from "@/components/blocks/hero";
-import MainFeatures from "@/components/blocks/main-features";
-import Pricing from "@/components/blocks/pricing";
-import CallToAction from "@/components/blocks/call-to-action";
+"use client";
+
+import React from "react";
+import { SparklesCore } from "@/components/ui/sparkles";
+import ClickSpark from "@/components/ui/click-sparks";
+import NewsletterForm from "@/components/forms/newsletter-form";
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Hero />
-      <MainFeatures />
-      <FeaturesSection />
-      <Pricing />
-      <CallToAction />
-    </div>
+    <ClickSpark>
+      <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+          Speetly
+        </h1>
+        <div className="w-[40rem] h-40 relative">
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+        <NewsletterForm />
+      </div>
+    </ClickSpark>
   );
 }
