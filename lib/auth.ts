7b0c3ea24282ee0626acedf0bdf6208c146e84f9
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-01 18:08:39 +0100

    feat: session better auth connect

---
+++ lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import db from "@/db";
+import { user, session, account, verification } from "@/db/auth-schema";
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "mysql",
+        schema: {
+            user,
+            session,
+            account,
+            verification,
+        },
+    }),
+
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+    },
+});
