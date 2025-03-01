6942ba7a2121d8f6f7d2588702c193c35be0b37e
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-01 18:06:16 +0100

    feat: experimental auth

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"],
+
   },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
-    ];
+  experimental: {
+    authInterrupts: true,
   },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true, 
 };
 
 export default nextConfig;
