fa2136bd920b86eac1d262cf60f2f0cf689939fa
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-03 12:15:45 +0100

    fix: key dev stripe test

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,5 @@
 import Stripe from "stripe";
 
-console.log(process.env.STRIPE_SECRET_KEY, "process.env.STRIPE_SECRET_KEY");
-
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-02-24.acacia",
 });
