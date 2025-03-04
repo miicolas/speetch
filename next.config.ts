0273c79907d7d9b6a2c80407648b4da905b86b50
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-04 20:14:55 +0100

    fix: domain allow

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "avatars.githubusercontent.com", "midday.ai"],
-
+    domains: [
+      "images.unsplash.com",
+      "avatars.githubusercontent.com",
+      "midday.ai",
+      "github.githubassets.com",
+    ],
   },
   experimental: {
     authInterrupts: true,
