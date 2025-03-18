a5ca5bd96db761c90b6f4d82c2be9abebacce1da
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-18 19:55:36 +0100

    fix: layout member

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import Footer from "@/components/blocks/footer";
 import Header from "@/components/blocks/header";
 export const metadata: Metadata = {
-  title: "Speetch",
-  description: "Speetch is a platform that allows you to create and manage your clients, projects, and tasks.",
+  title: "Speetly",
+  description: "Speetly is a platform that allows you to create and manage your clients, projects, and tasks.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
